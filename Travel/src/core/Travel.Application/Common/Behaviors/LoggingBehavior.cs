e3b56a5fa8e32f4483ab80f413119afcc52345d0
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-22 13:52:33 -0400

    feat: add LoggingBehavior

--- Travel/src/core/Travel.Application/Common/Behaviors/LoggingBehavior.cs
+++ Travel/src/core/Travel.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Travel.Application.Common.Behaviors
 {
-    internal class LoggingBehavior
+    public class LoggingBehavior<IRequest> : IRequestPreProcessor<IRequest>
     {
+        private readonly ILogger _logger;
+
+        public LoggingBehavior(ILogger<IRequest> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Process(IRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(IRequest).Name;
+            _logger.LogInformation("Travel Request: {@Request}", requestName, request);
+        }
     }
 }
