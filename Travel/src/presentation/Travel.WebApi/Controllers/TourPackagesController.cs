c028b99190e89f5a4e38cd8656bd82aa292b207f
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-21 20:30:04 -0400

    chore: trying dotnet formatting

---
+++ Travel/src/presentation/Travel.WebApi/Controllers/TourPackagesController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Travel.Data.Contexts;
+using Travel.Domain.Entities;
+
+namespace Travel.WebApi.Controllers {
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TourPackagesController : ControllerBase {
+
+
+
+    }
+}
