c39576f44cc10adb1036d74c4ca9f83c106366e7
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-22 20:52:04 -0400

    feat(core): add ICsvFileBuilder contract

---
+++ Travel/src/core/Travel.Application/Common/Interfaces/ICsvFileBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Travel.Application.Common.Interfaces
+{
+    public interface ICsvFileBuilder
+    {
+        byte[] BuildTourPackagesFile(IEnumerable<TourPackageRecord> records);
+    }
+}
