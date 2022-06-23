b295a40104cd7ee05515059e816d0dac14758a93
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-23 13:56:54 -0400

    feat(core): add query handler for ExportTours

---
+++ Travel/src/core/Travel.Application/TourLists/Queries/ExportTours/ExportToursQuery.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Travel.Application.Common.Interfaces;
+
+namespace Travel.Application.TourLists.Queries.ExportTours
+{
+    public class ExportToursQuery : IRequest<ExportToursVm>
+    {
+        public int ListId { get; set; }
+    }
+
+    public class ExportToursQueryHandler : IRequestHandler<ExportToursQuery, ExportToursVm>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+        private readonly ICsvFileBuilder _fileBuilder;
+
+        public ExportToursQueryHandler(IMapper mapper, IApplicationDbContext context, ICsvFileBuilder fileBuilder)
+        {
+            _context = context;
+            _mapper = mapper;
+            _fileBuilder = fileBuilder;
+        }
+
+        public async Task<ExportToursVm> Handle(ExportToursQuery request, CancellationToken cancellationToken)
+        {
+            var records = await _context.TourPackages
+                .Where(t => t.ListId == request.ListId)
+                .ProjectTo<TourPackageRecord>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            var vm = new ExportToursVm
+            {
+                Content = _fileBuilder.BuildTourPackagesFile(records.AsEnumerable()),
+                ContentType = "text/csv",
+                FileName = "TourPackages.csv"
+            };
+            return await Task.FromResult(vm);
+        }
+    }
+}
