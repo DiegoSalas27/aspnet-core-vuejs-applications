5553c25fdff573a0aaafd0f1fa59c6188e86e239
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-23 10:42:45 -0400

    refactor(core): remove unused usings

--- Travel/src/core/Travel.Application/Dtos/Tour/TourListDto.cs
+++ Travel/src/core/Travel.Application/Dtos/Tour/TourListDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Travel.Application.Common.Mappings;
 using Travel.Domain.Entities;
 
