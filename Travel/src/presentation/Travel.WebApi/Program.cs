87f362aaa5ff2e628522fbb2e0addda3f154de49
dominicsc2 <dominicsc2hs@gmail.com>
2022-06-24 12:24:14 -0400

    feat(api): add Serilog package to use as default Logger

--- Travel/src/presentation/Travel.WebApi/Program.cs
+++ Travel/src/presentation/Travel.WebApi/Program.cs
@@ -1,61 +1,106 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
 using Travel.Application;
 using Travel.Data;
 using Travel.Shared;
 using Travel.WebApi.Filters;
 using Travel.WebApi.Helpers;
 
-var builder = WebApplication.CreateBuilder(args);
+var name = Assembly.GetExecutingAssembly().GetName();
 
-// Add services to the container.
-builder.Services.AddApplication();
-builder.Services.AddInfrastructureData();
-builder.Services.AddInfrastructureShared(builder.Configuration);
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddControllers();
-builder.Services.AddControllersWithViews(options => options
-    .Filters.Add(new ApiExceptionFilter()));
-builder.Services.Configure<ApiBehaviorOptions>(options => options
-    .SuppressModelStateInvalidFilter = true);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { c.OperationFilter<SwaggerDefaultValues>(); });
-builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-builder.Services.AddApiVersioning(config =>
-{
-    config.DefaultApiVersion = new ApiVersion(1, 0);
-    config.AssumeDefaultVersionWhenUnspecified = true;
-    config.ReportApiVersions = true;
-});
-builder.Services.AddVersionedApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
+Log.Logger = new LoggerConfiguration() // Logger Configuration should be done in appsettings.json
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .Enrich.WithExceptionDetails()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", $"{name.Name}")
+    .Enrich.WithProperty("Assembly", $"{name.Version}")
+    .WriteTo.SQLite(
+            Environment.CurrentDirectory + @"\Logs\log.db",
+            restrictedToMinimumLevel: LogEventLevel.Information,
+            storeTimestampInUtc: true)
+    .WriteTo.File(
+            new CompactJsonFormatter(),
+            Environment.CurrentDirectory + @"\Logs\log.json",
+            rollingInterval: RollingInterval.Day,
+            restrictedToMinimumLevel: LogEventLevel.Information)
+    .WriteTo.Console()
+    .CreateLogger();
 
-var app = builder.Build();
-var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+    Log.Information("Starting host");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructureData();
+    builder.Services.AddInfrastructureShared(builder.Configuration);
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddControllers();
+    builder.Services.AddControllersWithViews(options => options
+        .Filters.Add(new ApiExceptionFilter()));
+    builder.Services.Configure<ApiBehaviorOptions>(options => options
+        .SuppressModelStateInvalidFilter = true);
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c => { c.OperationFilter<SwaggerDefaultValues>(); });
+    builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+    builder.Services.AddApiVersioning(config =>
     {
-        foreach (var description in provider.ApiVersionDescriptions)
-        { 
-            c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-        }
+        config.DefaultApiVersion = new ApiVersion(1, 0);
+        config.AssumeDefaultVersionWhenUnspecified = true;
+        config.ReportApiVersions = true;
     });
+    builder.Services.AddVersionedApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+            }
+        });
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
